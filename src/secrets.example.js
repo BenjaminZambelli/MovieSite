5f30a87a59214bff1d2697bfd48cfb8719baa809
Benjamin Zambelli <benjacxy@gmail.com>
2022-05-28 17:38:51 -0300

    development environment setup

---
+++ src/secrets.example.js
@@ -0,0 +1,6 @@
+// Create a file named secrets.js with your API KEY
+
+const API_KEY = 'Your key here';
+
+// This works for me so no one can see my apikey in the GitHub repo.
+// This doesn't actually prevent hiding it in practice.
